55d34dd425efb38b20eec5230b25a1433c939c3b
kingcheung98 <kingkobe9818@gmail.com>
2020-09-28 13:31:16 +0800

    times required

--- guess-number.py
+++ guess-number.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0 
 while True:
+	count += 1 #count + 1
 	num = input('guess the number:')
 	num = int(num)
 	if num == r:
 		print('bomb')
+		print('this is', count, 'times')
 		break
 	elif num < r:
 		print('try a bigger number')
 	elif num > r: 
 		print('try a smaller number')
+	print('this is', count, 'times')
